da2a491762055e3e30e9ca8650cb0faccf4270dc
astrit spanca <aspanca@ikub.local>
2022-04-05 10:30:32 +0200

    last fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout } from "./components/Layout";
+import { Layout } from "./components";
 import { AuthProvider, MaterialUiProvider, NotificationProvider } from "./providers";
 
 const App = () => {
--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -3,12 +3,12 @@ import React, {
   useCallback,
   useContext,
   useEffect,
-  useState,
+  useReducer,
 } from "react";
 import { SnackbarUtils } from "../ui";
 
 import { loginRequest } from "../http";
-import { decodeToken, fromJson, storage, fetchCredentials, setCredentials } from "../utils";
+import { decodeToken, fromJson, storage, setCredentials, fetchCredentials } from "../utils";
 
 export const Context = createContext({
   token: "",
@@ -16,44 +16,117 @@ export const Context = createContext({
   setAutoRefetching: () => {},
 });
 
+const initialState = {
+  token: "",
+  decoded: "",
+  loading: false,
+  autoRefetching: Boolean(fromJson(storage.get("autoRefetching"))),
+  instance: fetchCredentials().instance,
+  username: fetchCredentials().username,
+  password: fetchCredentials().password,
+};
+
+const ActionTypes = {
+  START_LOADING: "START_LOADING",
+  STOP_LOADING: "STOP_LOADING",
+  SET_CREDENTIALS: "SET_CREDENTIALS",
+  SET_AUTO_REFETCHING: "SET_AUTO_REFETCHING",
+  INPUT_CHANGE: "INPUT_CHANGE",
+  SET_TOKEN: "SET_TOKEN",
+  SET_DECODED_TOKEN: "SET_DECODED_TOKEN",
+};
+
+const reducer = (state, action) => {
+    console.log(action)
+  switch (action.type) {
+    case ActionTypes.START_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ActionTypes.STOP_LOADING:
+      return {
+        ...state,
+        loading: false,
+      };
+    case ActionTypes.SET_TOKEN:
+      return {
+        ...state,
+        token: action.payload.token,
+      };
+    case ActionTypes.SET_DECODED_TOKEN:
+      return {
+        ...state,
+        decoded: action.payload.decoded,
+      };
+    case ActionTypes.SET_AUTO_REFETCHING:
+      return {
+        ...state,
+        autoRefetching: !state.autoRefetching,
+      };
+    case ActionTypes.INPUT_CHANGE:
+      return {
+        ...state,
+        [action.payload.target.name]: action.payload.target.value,
+      };
+    default:
+      return state;
+  }
+};
+
 export const useAuthContext = () => useContext(Context);
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState("");
-  const [decoded, setDecoded] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [autoRefetching, setAutoRefetching] = useState(
-    Boolean(fromJson(storage.get("autoRefetching")))
-  );
-  const [{ instance, username, password }, setState] = useState(
-    fetchCredentials()
-  );
+  const [
+    { token, decoded, loading, autoRefetching, instance, username, password },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const login = useCallback(async () => {
+
     if (!instance || !username || !password) return;
 
     setCredentials({ instance, username, password });
 
     try {
-      setLoading(true);
+      dispatch({
+        type: ActionTypes.START_LOADING,
+      });
       const res = await loginRequest({ instance, username, password });
-      setToken(res.data);
-      setDecoded(decodeToken(res.data));
+
+      dispatch({
+        type: ActionTypes.SET_TOKEN,
+        payload: { token: res.data },
+      });
+      dispatch({
+        type: ActionTypes.SET_DECODED_TOKEN,
+        payload: { decoded: decodeToken(res.data) },
+      });
       SnackbarUtils.success("Authentication successful");
     } catch (error) {
+      dispatch({
+        type: ActionTypes.SET_TOKEN,
+        payload: { token: "" },
+      });
+      dispatch({
+        type: ActionTypes.SET_DECODED_TOKEN,
+        payload: { decoded: "" },
+      });
       SnackbarUtils.error("Authentication failed");
     } finally {
-      setLoading(false);
+      dispatch({
+        type: ActionTypes.STOP_LOADING,
+      });
     }
   }, [instance, username, password]);
 
   const handleChange = (e) => {
-    setState((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    dispatch({ type: ActionTypes.INPUT_CHANGE, payload: e });
   };
 
   const toggleAutoRefetching = () => {
     storage.set("autoRefetching", !autoRefetching);
-    setAutoRefetching((prevState) => !prevState);
+    dispatch({ type: ActionTypes.SET_AUTO_REFETCHING });
   };
 
   useEffect(() => {
@@ -66,6 +139,8 @@ export const AuthProvider = ({ children }) => {
 
     return () => clearInterval(interval);
   }, [autoRefetching, login]);
+
+  console.log(token);
 
   return (
     <Context.Provider
